dcccf2adf4c1fd8e57768f2d8be06d2e5b7a350a
Popagopa <popagopa@gmail.com>
2025-11-04 04:02:07 +1000

    Реализовал функцию DrawCalendar пакета calendar

--- calendar/calendar.go
+++ calendar/calendar.go
@@ -5,9 +5,68 @@ import (
 	"time"
 )
 
+func isLeap(year int) bool {
+	return (year%4 == 0 && year%100 != 0) || (year%400 == 0)
+}
+func howDaysInMonyh(m int, flag bool) int {
+	switch m {
+	case 1:
+		return 31
+	case 2:
+		if flag {
+			return 29
+		}
+		return 28
+	case 3:
+		return 31
+	case 4:
+		return 30
+	case 5:
+		return 31
+	case 6:
+		return 30
+	case 7:
+		return 31
+	case 8:
+		return 31
+	case 9:
+		return 30
+	case 10:
+		return 31
+	case 11:
+		return 30
+	}
+	return 31
+}
+func meet(firstDay time.Time, numDays int) []string {
+	w := int(firstDay.Weekday())
+	res := make([]string, 0, numDays)
+	for i := 0; i < w; i++ {
+		res = append(res, "  ")
+	}
+	for i := 1; i <= numDays; i++ {
+		if i < 10 {
+			res = append(res, fmt.Sprintf("0%d", i))
+			continue
+		}
+		res = append(res, fmt.Sprintf("%d", i))
+	}
+	return res
+}
 func DrawCalendar(t time.Time) {
 	y := t.Year()
 	m := t.Month()
-	fmt.Printf("----%d----\n", y)
-	fmt.Println(m)
+	firstDayMonth := time.Date(y, m, 1, 0, 0, 0, 1, time.UTC)
+	days := meet(firstDayMonth, howDaysInMonyh(int(m), isLeap(y)))
+	fmt.Printf("--------%d--------\n", y)
+	fmt.Println("Su Mo Tu We Th Fr Sa")
+	for i, day := range days {
+		fmt.Print(day)
+		if i%7 == 0 {
+			fmt.Print("\n")
+			continue
+		}
+		fmt.Print(" ")
+	}
+
 }
